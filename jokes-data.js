2774eff62151e099b1786c9389e283f953a2a3b3
JokeOfTheDay <maartenvanbosbeke@gmail.com>
2026-08-20 08:51:10 +0200

    first ever actual production data joke

--- jokes-data.js
+++ jokes-data.js
@@ -1,33 +1,13 @@
 const jokes = [
   {
-    date: "2026-08-19",
-    setup: "Why don't scientists trust atoms?",
-    punchline: "Because they make up everything!"
-  },
-  {
     date: "2026-08-18",
-    setup: "Why did the bicycle fall over?",
-    punchline: "Because it was two-tired."
-  },
-  {
-    date: "2026-08-17",
-    setup: "I only know 25 letters of the alphabet.",
-    punchline: "I don't know y."
-  },
-  {
-    date: "2026-08-16",
-    setup: "What do you call fake spaghetti?",
-    punchline: "An impasta."
-  },
-  {
-    date: "2026-08-15",
-    setup: "Why did the scarecrow win an award?",
-    punchline: "Because he was outstanding in his field."
+    setup: "Why don't dwarves shop at Aldi?",
+    punchline: "Because they're Lidl people."
   },
   {
     date: "2026-04-01",
     setup: "Sorry, no older jokes available :(",
-    punchline: "April Fool's! Hahahahahahahahaha but seriously, no older jokes after this one."
+    punchline: "April Fool's! Hahahahahahahahaha but seriously, no older jokes before this one."
   }
   
 ];
